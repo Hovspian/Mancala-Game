6168dbbf61928e9253876678d82ff04b5aaf6d2a
Hovsep git <hovseplalikian@gmail.com>
2017-12-08 16:15:12 -0800

    Pit stones

--- mancala/src/PitIcon.java
+++ mancala/src/PitIcon.java
@@ -27,11 +27,30 @@ public class PitIcon extends AbstractPitIcon
 		g2.draw(shape);
 
 		g2.setColor(getStoneColor());
-		g2.fill(new Ellipse2D.Double(getIconWidth() / 2 - 5, getIconHeight() / 2 - 5, 10, 10));
+		drawStones(g2);
 	}
 
 	public int getPosition()
 	{
 		return position;
+	}
+	
+	private void drawStones(Graphics2D g2)
+	{
+		final int STONE_SIZE = BoardStrategy.STONE_SIZE;
+		
+		int stonesDrawn = 0;
+		int xPos = getIconWidth() / 6;
+		int yPos = getIconHeight() / 4;
+		while (stonesDrawn < getStones())
+		{
+			xPos = getIconWidth() / 10 + stonesDrawn % 7 * STONE_SIZE / 2 + STONE_SIZE;
+			yPos = getIconHeight() / 4 + stonesDrawn / 7 * STONE_SIZE / 2 + STONE_SIZE;
+			
+			g2.fill(new Ellipse2D.Double(xPos + ((stonesDrawn % 7) * (STONE_SIZE / 2)), yPos + stonesDrawn / 7 * STONE_SIZE / 2, STONE_SIZE, STONE_SIZE));			
+
+			stonesDrawn++;
+		}
+
 	}
 }
